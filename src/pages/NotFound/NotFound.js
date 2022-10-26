2bf8606f008466a611840ab6e2acf38540215973
zeon-X <mdshefatzeon@gmail.com>
2022-10-27 00:03:28 +0600

    asdasd

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="h-screen flex justify-center items-center ">
-      <p>NOT FOUND</p>
+    <div className="h-screen flex justify-center items-center flex-col">
+      <img
+        style={{ width: "200px", height: "200px" }}
+        src="https://cdn-icons-png.flaticon.com/512/5545/5545083.png"
+        alt=""
+      />
+      <p className="font-bold" style={{ color: "#8366DD" }}>
+        NOT FOUND
+      </p>
     </div>
   );
 };
